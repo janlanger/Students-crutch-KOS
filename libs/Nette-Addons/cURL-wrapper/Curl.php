32563179e0ab61c844101ec351a1d2fcfac60b3e
Quinix <quinix@quinix.cz>
2010-10-12 15:51:57 +0200

    Curl wrapper fix

--- libs/Nette-Addons/cURL-wrapper/Curl.php
+++ libs/Nette-Addons/cURL-wrapper/Curl.php
@@ -1247,7 +1247,7 @@ class CurlException extends Exception
 
 
 
-	public function __construct($message, $code, $response)
+	public function __construct($message, $code=0, $response=NULL)
 	{
 		parent::__construct($message, $code);
 
