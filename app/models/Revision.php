31530358520348bab3e87b8d47356a1e324da1b1
Quinix <quinix@quinix.cz>
2010-11-07 15:09:47 +0100

    Revision create model support

--- app/models/Revision.php
+++ app/models/Revision.php
@@ -21,5 +21,42 @@ class Revision extends Model {
         return $q->execute()->setRowClass(get_called_class())->fetchAssoc('rev_id');
     }
 
+    public static function getAvaiableTables($database=NULL) {
+        if($database==NULL) $database=NEnvironment::getConfig('xml')->liveDatabase;
+
+        $databaseManager=NEnvironment::getContext()->getService('IDatabaseManager');
+        return $databaseManager->getTables($database);
+    }
+
+    public static function create($name,$app_id,$isMain,$database,$tables,$from=NULL) {
+        if(dibi::select('count(*)')->from(":main:revision")->where(array("app_id"=>$app_id,'alias'=>$name))->execute()->fetchSingle()) {
+            throw new ModelException('Revizi s tímto názvem nelze vytvořit. Pravděpodobně již existuje.');
+        }
+        if(dibi::select('count(*)')->from(":main:revision")->where(array("app_id"=>$app_id,'isMain'=>$isMain))->execute()->fetchSingle()) {
+            throw new ModelException('Nelze vytvořit více revizí označených jako výchozí.');
+        }
+
+
+        if($from==NULL) $from=NEnvironment::getConfig('xml')->liveDatabase;
+
+        $databaseManager=NEnvironment::getContext()->getService('IDatabaseManager');
+        try{
+            $databaseManager->createRevision($from,$database,$tables);
+            dibi::insert(":main:revision", array(
+                "app_id"=>$app_id,
+                "alias"=>$name,
+                "db_name"=>$database,
+                "isMain"=>$isMain
+            ))->execute();
+            return TRUE;
+        } catch (DatabaseManagerException $e) {
+            throw new ModelException('Unable to create revision '.$name.'. '.$e->getMessage(), NULL, $e);
+        } catch (DibiException $e) {
+            throw new ModelException($e->getMessage(), $e->getCode(), $e);
+        }
+
+
+    }
+
 }
 ?>
