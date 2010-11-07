455113434a1d4c1eb065da27512f0da13031b33b
Quinix <quinix@quinix.cz>
2010-11-07 17:29:59 +0100

    Revision update and delete

--- app/models/Revision.php
+++ app/models/Revision.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -12,51 +13,121 @@
 class Revision extends Model {
 
     public static function find($where = NULL, $order = NULL, $offset = NULL, $limit = NULL) {
-        $q=dibi::select("*")->from("[:main:revision]");
-        if($where!=NULL) $q->where ($where);
-        if($order!=NULL) $q->orderBy ($order);
-        if($limit!=NULL) $q->limit ($limit);
-        if($offset!=NULL) $q->offset ($offset);
+        $q = dibi::select("*")->from("[:main:revision]");
+        if ($where != NULL)
+            $q->where($where);
+        if ($order != NULL)
+            $q->orderBy($order);
+        if ($limit != NULL)
+            $q->limit($limit);
+        if ($offset != NULL)
+            $q->offset($offset);
 
         return $q->execute()->setRowClass(get_called_class())->fetchAssoc('rev_id');
     }
 
     public static function getAvaiableTables($database=NULL) {
-        if($database==NULL) $database=NEnvironment::getConfig('xml')->liveDatabase;
+        if ($database == NULL)
+            $database = NEnvironment::getConfig('xml')->liveDatabase;
 
-        $databaseManager=NEnvironment::getContext()->getService('IDatabaseManager');
+        $databaseManager = NEnvironment::getContext()->getService('IDatabaseManager');
         return $databaseManager->getTables($database);
     }
 
-    public static function create($name,$app_id,$isMain,$database,$tables,$from=NULL) {
-        if(dibi::select('count(*)')->from(":main:revision")->where(array("app_id"=>$app_id,'alias'=>$name))->execute()->fetchSingle()) {
+    public static function create($name, $app_id, $isMain, $database, $tables, $from=NULL) {
+        if (!count($tables)) {
+            throw new ModelException('Revize musí obsahovat alespoň jednu tabulku.');
+        }
+
+        if (dibi::select('count(*)')->from(":main:revision")->where(array("app_id" => $app_id, 'alias' => $name))->execute()->fetchSingle()) {
             throw new ModelException('Revizi s tímto názvem nelze vytvořit. Pravděpodobně již existuje.');
         }
-        if(dibi::select('count(*)')->from(":main:revision")->where(array("app_id"=>$app_id,'isMain'=>$isMain))->execute()->fetchSingle()) {
-            throw new ModelException('Nelze vytvořit více revizí označených jako výchozí.');
+
+        if ($isMain) {
+            if (dibi::select('count(*)')->from(":main:revision")->where(array("app_id" => $app_id, 'isMain' => $isMain))->execute()->fetchSingle()) {
+                throw new ModelException('Nelze vytvořit více revizí označených jako výchozí.');
+            }
         }
 
+        if ($from == NULL)
+            $from = NEnvironment::getConfig('xml')->liveDatabase;
 
-        if($from==NULL) $from=NEnvironment::getConfig('xml')->liveDatabase;
-
-        $databaseManager=NEnvironment::getContext()->getService('IDatabaseManager');
-        try{
-            $databaseManager->createRevision($from,$database,$tables);
+        $databaseManager = NEnvironment::getContext()->getService('IDatabaseManager');
+        try {
+            $databaseManager->createRevision($from, $database, $tables);
             dibi::insert(":main:revision", array(
-                "app_id"=>$app_id,
-                "alias"=>$name,
-                "db_name"=>$database,
-                "isMain"=>$isMain
+                "app_id" => $app_id,
+                "alias" => $name,
+                "db_name" => $database,
+                "isMain" => $isMain
             ))->execute();
             return TRUE;
         } catch (DatabaseManagerException $e) {
-            throw new ModelException('Unable to create revision '.$name.'. '.$e->getMessage(), NULL, $e);
+            throw new ModelException('Unable to create revision ' . $name . '. ' . $e->getMessage(), NULL, $e);
         } catch (DibiException $e) {
             throw new ModelException($e->getMessage(), $e->getCode(), $e);
         }
+    }
 
+    public function getTables() {
+        $databaseManager = NEnvironment::getContext()->getService('IDatabaseManager');
+        try {
+            return $databaseManager->getTables($this->db_name);
+        } catch (DatabaseManagerException $e) {
+            throw new ModelException('Nepodařilo se získat seznam tabulek. ' . $e->getMessage(), NULL, $e);
+        }
+    }
 
+    public function getDatabaseSize() {
+        return $databaseManager = NEnvironment::getContext()->getService('IDatabaseManager')->getDatabaseSize($this->db_name);
+    }
+
+    public function delete() {
+        $databaseManager = NEnvironment::getContext()->getService('IDatabaseManager');
+        try {
+            $databaseManager->dropDatabase($this->db_name);
+            dibi::delete(':main:revision')->where(array("rev_id" => $this->rev_id))->execute();
+        } catch (DatabaseManagerException $e) {
+            throw new ModelException('Nepodařilo se smazat databázi ' . $name . '. ' . $e->getMessage(), NULL, $e);
+        } catch (DibiException $e) {
+            throw new ModelException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function setValues($values) {
+        if (dibi::select('count(*)')->from(":main:revision")
+                        ->where("app_id=%s", $this->app_id)
+                        ->and('alias=%s', $values['name'])
+                        ->and('[alias]!=%s', $this->alias)
+                        ->execute()->fetchSingle()) {
+            throw new ModelException('Revizi s tímto názvem nelze vytvořit. Pravděpodobně již existuje.');
+        }
+
+        if ($values['isMain']) {
+            if (dibi::select('count(*)')->from(":main:revision")
+                            ->where("app_id=%s", $this->app_id)
+                            ->and('isMain=%b', $values['isMain'])
+                            ->and("[alias]!=%s", $this->alias)
+                            ->execute()->fetchSingle()) {
+                throw new ModelException('Nelze vytvořit více revizí označených jako výchozí.');
+            }
+        }
+        $this->alias = $values['name'];
+        $this->isMain = $values['isMain'];
+        return $this;
+    }
+
+    public function save() {
+        try {
+            dibi::update(":main:revision", array(
+                "alias"=>$this->alias,
+                "isMain"=>$this->isMain
+            ))->where("rev_id=%i",$this->rev_id)->execute();
+        } catch (DibiException $e) {
+            throw new ModelException('Nepodařilo se uložit data.', NULL, $e);
+        }
     }
 
 }
+
 ?>
--- app/presenters/RevisionPresenter.php
+++ app/presenters/RevisionPresenter.php
@@ -8,80 +8,129 @@
 class RevisionPresenter extends BasePresenter {
 
     /** @persistent */
-    public $app_id=0;
+    public $app_id = 0;
 
-	
+    public function actionDefault($app_id) {
+        $application = @reset(Application::find(array("app_id" => $app_id)));
+        $this['header']->addTitle("Správa revizí pro aplikaci " . $application->name);
+    }
 
-	public function actionDefault($app_id) {
-            $application=@reset(Application::find(array("app_id"=>$app_id)));
-            $this['header']->addTitle("Správa revizí pro aplikaci ".$application->name);
-	}
+    public function actionCreate() {
+        $this['header']->addTitle('Vytvoření revize');
+    }
 
-        public function actionCreate() {
-            $this['header']->addTitle('Vytvoření revize');
+    public function actionEdit($rev_id) {
+        $this['header']->addTitle('Úprava revize');
+        $rev = Revision::find(array("app_id" => $this->app_id, "rev_id" => $rev_id));
+        if (count($rev) != 1) {
+            $this->flashMessage('Neznámá revize.', 'error');
+            $this->redirect('default');
         }
+        $rev = @reset($rev);
+        $this['editForm']->setDefaults(array(
+            "name"=>$rev->alias,
+            "rev_id"=>$rev_id,
+            "isMain"=>$rev->isMain
+        ));
+        $this->template->revision=$rev;
+    }
 
-        public function actionEdit() {
-
-        }
-
-        public function actionDelete() {
-
-        }
-
-
-        public function createComponentDatagrid($name) {
-            
-            $grid=new Datagrid($this, $name);
-            $grid->setDataTable(":main:revision");
-            $grid->setColumns(array("rev_id"=>'ID#','db_name'=>'Databáze','alias'=>'Alias','created_time'=>'Čas vytvoření','isMain'=>'Výchozí'));
-            $grid->getSql()->where(array("app_id"=>$this->app_id));
-            $grid->setColumnFormat('created_time', DatagridFormatter::DATE);
-            $grid->setColumnFormat('isMain', DatagridFormatter::CHECKBOX_YES_NO);
-            $grid->addAction(array('action'=>'Revision:edit','param'=>'rev_id'), 'Upravit','edit');
-            $grid->addAction(array('action'=>'Revision:delete','param'=>'rev_id'), 'Smazat','delete')->setConfirmQuestion('Smazat', 'Opravdu chcete smazat tuto revizi? Bude odstraněna celá svázaná databáze.');
-        }
-
-        protected function createComponentCreateForm($name) {
-            $form=new NAppForm($this, $name);
-            $group=$form->addGroup('Základní nastavení');
-            $form->addText('name','Název revize')->setRequired('Vyplňte prosím název revize');
-            //$form->addText('db_name','Jméno databáze')->setRequired('Vyplňte prosím jméno databáze'); //automat
-            $form->addCheckbox('isMain','Používat jako výchozí');
-
-            $group=$form->addGroup('Obsah revize')->setOption('description', 'Vyberte tabulky, které chcete zkopírovat do revize.');
-
-            $tables=Revision::getAvaiableTables();
-            foreach($tables as $table) {
-                $form->addCheckbox($table, $table);
-            }
-            $form->addGroup();
-            $form->addHidden("app_id",  $this->app_id);
-            $form->addSubmit('s','Odeslat')->onClick[]=callback($this,'createRevision');
-
-            return $form;
-
-        }
-
-        public function createRevision(NSubmitButton $bnt) {
-            $values=$bnt->getForm()->getValues();
-
-            $tables=Revision::getAvaiableTables();
-            $tbl=array();
-            foreach($values as $key=>$value) {
-                if(in_array($key, $tables) && $value == TRUE) {
-                    $tbl[]=$key;
-                }
-            }
-            $database_name="rozvrh_".NString::webalize(@reset(Application::find(array("app_id"=>$values['app_id'])))->name)."_".$values['name'].'_'.date("Ymd");
+    public function actionDelete($rev_id) {
+        $rev = Revision::find(array("app_id" => $this->app_id, "rev_id" => $rev_id));
+        if (count($rev) != 1) {
+            $this->flashMessage('Neznámá revize.', 'error');
+        } else {
             try {
-                Revision::create($values['name'],$values['app_id'],$values['isMain'],$database_name,$tbl);
-                $this->flashMessage('Revize byla úspěšně vytvořena.', 'success');
-                $this->redirect('default');
-            }
-            catch (ModelException $e) {
-                $this->flashMessage('Revizi se nepodařilo vytvořit. Chyba: '.$e->getMessage(), 'error');
+                $rev = @reset($rev);
+                $rev->delete();
+                $this->flashMessage('Revize byla smazána.', 'success');
+            } catch (ModelException $e) {
+                $this->flashMessage($e->getMessage(), 'error');
                 NDebug::log($e);
             }
         }
+        $this->redirect('default');
+    }
+
+    public function createComponentDatagrid($name) {
+
+        $grid = new Datagrid($this, $name);
+        $grid->setDataTable(":main:revision");
+        $grid->setColumns(array("rev_id" => 'ID#', 'db_name' => 'Databáze', 'alias' => 'Alias', 'created_time' => 'Čas vytvoření', 'isMain' => 'Výchozí'));
+        $grid->getSql()->where(array("app_id" => $this->app_id));
+        $grid->setColumnFormat('created_time', DatagridFormatter::DATE);
+        $grid->setColumnFormat('isMain', DatagridFormatter::CHECKBOX_YES_NO);
+        $grid->addAction(array('action' => 'Revision:edit', 'param' => 'rev_id'), 'Upravit', 'edit');
+        $grid->addAction(array('action' => 'Revision:delete', 'param' => 'rev_id'), 'Smazat', 'delete')->setConfirmQuestion('Smazat', 'Opravdu chcete smazat tuto revizi? Bude odstraněna celá svázaná databáze.');
+    }
+
+    protected function createComponentCreateForm($name) {
+        $form = new NAppForm($this, $name);
+        $group = $form->addGroup('Základní nastavení');
+        $form->addText('name', 'Název revize')->setRequired('Vyplňte prosím název revize');
+        //$form->addText('db_name','Jméno databáze')->setRequired('Vyplňte prosím jméno databáze'); //automat
+        $form->addCheckbox('isMain', 'Používat jako výchozí');
+
+        $group = $form->addGroup('Obsah revize')->setOption('description', 'Vyberte tabulky, které chcete zkopírovat do revize.');
+
+        $tables = Revision::getAvaiableTables();
+        foreach ($tables as $table) {
+            $form->addCheckbox($table, $table);
+        }
+        $form->addGroup();
+        $form->addHidden("app_id", $this->app_id);
+        $form->addSubmit('s', 'Odeslat')->onClick[] = callback($this, 'createRevision');
+
+        return $form;
+    }
+
+    public function createRevision(NSubmitButton $bnt) {
+        $values = $bnt->getForm()->getValues();
+
+        $tables = Revision::getAvaiableTables();
+        $tbl = array();
+        foreach ($values as $key => $value) {
+            if (in_array($key, $tables) && $value == TRUE) {
+                $tbl[] = $key;
+            }
+        }
+        $database_name = "rozvrh_" . NString::webalize(@reset(Application::find(array("app_id" => $values['app_id'])))->name) . "_" . $values['name'] . '_' . date("Ymd");
+        try {
+            Revision::create($values['name'], $values['app_id'], $values['isMain'], $database_name, $tbl);
+            $this->flashMessage('Revize byla úspěšně vytvořena.', 'success');
+            $this->redirect('default');
+        } catch (ModelException $e) {
+            $this->flashMessage('Revizi se nepodařilo vytvořit. Chyba: ' . $e->getMessage(), 'error');
+            NDebug::log($e);
+        }
+    }
+
+    protected function createComponentEditForm($name) {
+        $form=new NAppForm($this, $name);
+        $form->addText('name', 'Název revize')->setRequired('Vyplňte prosím název revize');
+        //$form->addText('db_name','Jméno databáze')->setRequired('Vyplňte prosím jméno databáze'); //automat
+        $form->addCheckbox('isMain', 'Používat jako výchozí');
+        $form->addHidden('rev_id');
+        $form->addSubmit('s','Odeslat')->onClick[]=callback($this,'editRevision');
+    }
+
+    public function editRevision(NSubmitButton $bnt) {
+        $values = $bnt->getForm()->getValues();
+        $revision = Revision::find(array("rev_id"=>$values['rev_id']));
+        if(count($revision)!=1) {
+            $this->flashMessage('Neznámá revize.', 'error');
+        } else {
+            $revision=@reset($revision);
+            try {
+                $revision->setValues($values)->save();
+                $this->flashMessage('Revize byla upravena.','success');
+                $this->redirect('default');
+            } catch (ModelException $e) {
+                $this->flashMessage($e->getMessage(),'error');
+                NDebug::log($e);
+            }
+        }
+
+    }
+
 }
