8c3cd37c8a1973a15a3f58ba6068c91b1a0d960c
Quinix <quinix@quinix.cz>
2011-03-12 19:39:43 +0100

    updated soap test script

--- soap/soap_test.php
+++ soap/soap_test.php
@@ -1,18 +1,23 @@
 <?php
 
-include "./bootstrap-soap.php";
+include "./bootstrap-soap.php";		//init prostredi (hlavne kvuli vecem jako Nette\Debug apod. - neni vubec nutne pouzivat
+
+header('Content-type: text/plain'); //kvuli srozumitelnejsimu zobrazeni vystupu var_dump
 
 $soap = new SoapClient(NULL, array(
-    "location" => 'http://bp.local/soap/',
-    "uri" => 'http://bp.local/soap/'));
+    "location" => 'http://kos.janlanger.cz/soap/',
+    "uri" => 'http://kos.janlanger.cz/soap/'));	//pripojeni k servise
 try {
-    $soap->authenticate('berlicka', 'test');
-    $soap->useRevision('testing');
+    $soap->authenticate('berlicka', 'test');	//identifikace a prihlaseni klienta
+    $soap->useRevision('live');					//vyber revize (nepovinne, pokud se neuvede, pouzije se vychozi revize)
 
-    $return = ($soap->getStudentsInfo(array('langeja1')));
+    $return = ($soap->getAllClasses());			//ziskani odpovedi metody s nazev getAllClases
     var_dump($return);
+
 } catch (SoapFault $e) {
+
     echo $soap->getLastError();
+	
 }
 
 ?>
